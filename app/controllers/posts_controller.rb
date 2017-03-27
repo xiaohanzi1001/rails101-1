f02da55a6627bc7d83cbbfd68d94d903290b0f02
Cong <xiaohanzi1001@gmail.com>
2017-03-27 22:45:57 +0200

    implement post#edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
 
+def index
+@posts = Post.all
+end
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -12,11 +16,32 @@ def create
   @post = Post.new(post_params)
   @post.group = @group
   @post.user = current_user
-  
+
 if @post.save
   redirect_to group_path(@group)
 else
 render :new
+end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  flash[:alert] = "Group deleted"
+  redirect_to account_posts_path
+end
+
+def edit
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+end
+
+def udpate
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to account_posts_path, notice: "Update Success"
+  else
+    render :edit
 end
 end
 
